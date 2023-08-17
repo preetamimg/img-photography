4e0fe10f8f3787264e0c49714aea12904624d309
ajaysainiimg <104185399+ajaysainiimg@users.noreply.github.com>
2023-08-17 17:46:29 +0530

    about page
    about page , about section design and counter section design with responsive, about owner offcanvas design with responsive

---
+++ assets/js/counter.js
@@ -0,0 +1,21 @@
+
+        // Counter To Count Number Visit
+        var a = 0;
+        $(window).scroll(function() {
+
+        var oTop = $('#counter').offset().top - window.innerHeight;
+        // Md.Asaduzzaman Muhid
+        if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter').each(function() {
+            var $this = $(this);
+            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                duration: 2000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+        a = 1;// Md.Asaduzzaman Muhid
+        }
+        });
